334b63ea3909f6a2e6dfec0f5e87b36a4f249897
Ana Belen Jover Ballester <anab.jover@gmail.com>
2022-06-02 11:23:00 +0200

    comment model

---
+++ models/Comment.model.js
@@ -0,0 +1,25 @@
+const { Schema, model } = require("mongoose");
+
+const commentSchema = new Schema(
+    {
+        videogame: {
+            type: Schema.Types.ObjectId,
+            ref: "games",
+        },        
+        username: {
+            type: Schema.Types.ObjectId,
+            ref: "user",
+        },
+        comment: {
+            type: String,
+            required: true,
+        }
+    },
+    {
+        timestamps: true,
+    }
+)
+
+const CommentModel = model("comment", commentSchema)
+
+module.exports = CommentModel
