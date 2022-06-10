862fcbe6828257646d745250c6099b3022a5532d
Ana Belen Jover Ballester <anab.jover@gmail.com>
2022-06-10 02:00:36 +0200

    no funciona edit profile

--- routes/uploader.routes.js
+++ routes/uploader.routes.js
@@ -5,7 +5,7 @@ const uploader = require("../middlewares/cloudinary")
 // esta ruta solo va a enviar una imagen a cloudinary y recibe el URL
 router.post("/", uploader.single("image"), (req, res, next) => {
     
-    console.log(req.file.path) // la imagen de cloudinary
+    console.log("ruta Post de uploader", req.file.path) // la imagen de cloudinary
 
     res.json(req.file.path)
 
